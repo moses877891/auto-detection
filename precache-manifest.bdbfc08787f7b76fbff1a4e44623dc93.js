8137f8df8bc5f64325b452011e23fc79f7f63e25
moses877891 <mosesantonymas@gmail.com>
2021-05-07 09:49:22 +0530

    Updates

---
+++ precache-manifest.bdbfc08787f7b76fbff1a4e44623dc93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec33ce996968b29db76ff1c1ca8ef565",
+    "url": "/auto-detection/index.html"
+  },
+  {
+    "revision": "f7444c1704a5f0d82d0c",
+    "url": "/auto-detection/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "9cc2d4d5bee41b45729d",
+    "url": "/auto-detection/static/js/2.9dd20c31.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/auto-detection/static/js/2.9dd20c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7444c1704a5f0d82d0c",
+    "url": "/auto-detection/static/js/main.69d0a87b.chunk.js"
+  },
+  {
+    "revision": "3358300b32c978cf49d8",
+    "url": "/auto-detection/static/js/runtime-main.0d305845.js"
+  }
+]);
